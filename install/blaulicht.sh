02cf573a5169768043a70ffae60656e401ceef53
Mik Müller <83001409+MikMuellerDev@users.noreply.github.com>
2025-09-23 18:44:03 +0200

    .

--- install/blaulicht.sh
+++ install/blaulicht.sh
@@ -8,7 +8,11 @@ xset s noblank
 #
 
 xrandr --newmode "1024x600_60.00"   49.00  1024 1064 1168 1312  600 603 613 624 -hsync +vsync
+xrandr --newmode "800x480_60.00"   29.50  800 824 896 992  480 483 493 500 -hsync +vsync
+
 xrandr --addmode VGA-1 "1024x600_60.00"
+xrandr --addmode VGA-1 "800x480_60.00"
+
 xrandr --output VGA-1 --mode "1024x600_60.00"
 
 # Launch blaulicht
