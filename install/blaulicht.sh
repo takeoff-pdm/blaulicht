66415d0706d0fa380e36b7670845e3e38a1ef5a1
Mik Müller <83001409+MikMuellerDev@users.noreply.github.com>
2025-09-23 18:41:59 +0200

    .

--- install/blaulicht.sh
+++ install/blaulicht.sh
@@ -5,6 +5,11 @@ xset -dpms
 xset s noblank
 
 # TODO: do something to relays?
+#
+
+xrandr --newmode "1024x600_60.00"   49.00  1024 1064 1168 1312  600 603 613 624 -hsync +vsync
+xrandr --addmode VGA-1 "1024x600_60.00"
+xrandr --output VGA-1 --mode "1024x600_60.00"
 
 # Launch blaulicht
 blaulicht
